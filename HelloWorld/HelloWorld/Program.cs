d0f67ad95da513bd9a3266fe6dfaad7a0143092c
osakipokima <osakipokima@gmail.com>
2021-02-03 23:15:10 -0500

    Numbers between 1-100 that are divisible by 3.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,47 +6,21 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Speed Limit:");
-            int speedLimit = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("How fast are you driving?");
-            int carSpeed = Convert.ToInt32(Console.ReadLine());
-
-
-            int dermitPoints = (carSpeed - speedLimit) / 5;
-
-            if(carSpeed > speedLimit && dermitPoints <= 12)
+            var counter = 0;
+            for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine("Dermit Points: " + dermitPoints);
-            }
-            else if(dermitPoints > 12){
-                Console.WriteLine("License Suspended");
+                if (i % 3 == 0)
+                {
+                    counter += 1;
+
+                }
 
             }
-            else
-            {
-                Console.WriteLine("Keep driving safely");
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine(counter);
 
 
         }
 
     }
-    }
+}
 
